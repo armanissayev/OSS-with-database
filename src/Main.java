8620a4ef3969b01ab06f88b10c6e25eee6968157
Arman Issayev <issayevarman@mail.ru>
2024-02-03 17:45:14 +0600

    New repository created

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
