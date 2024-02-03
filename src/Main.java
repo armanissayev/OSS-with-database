68318c692793688f3e399623b65ec3f1173db9fe
Arman Issayev <issayevarman@mail.ru>
2024-02-03 17:54:40 +0600

    Created entities, controllers, repositories

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 public class Main {
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "0000";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+
+            createUsersTable(connection);
+
+            UserRepository userRepository = new UserRepository(connection);
+            UserController userController = new UserController(userRepository);
+
+            runUserManagementApp(userController);
+
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
