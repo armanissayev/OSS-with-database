d27de8bbb9685cab8e1e354b30e5b31352f6c8b3
Dias06K <90494358+Dias06K@users.noreply.github.com>
2024-02-04 22:53:45 +0600

    Update Products.java

--- src/entities/Products.java
+++ src/entities/Products.java
@@ -1,4 +1,44 @@
 package entities;
 
 public class Products {
+
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
+
