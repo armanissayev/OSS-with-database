16114149031688e4dd18928f8d8758256d532844
Dias06K <90494358+Dias06K@users.noreply.github.com>
2024-02-04 22:55:11 +0600

    Update Order.java

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,4 +1,44 @@
 package entities;
 
 public class Order {
+
+    private User user;
+    private Product product;
+    private int quantity;
+    private double price;
+
+    // Getters
+    public User getUser() {
+        return user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
