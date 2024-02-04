85064d1541597a5d252ded9bf0e86028497d12e1
Dias06K <90494358+Dias06K@users.noreply.github.com>
2024-02-04 22:52:04 +0600

    Update User.java

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,4 +1,44 @@
 package entities;
 
 public class User {
+
+    private int id;
+    private String username;
+    private int age;
+    private double balance;
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
 }
