16cda63fd321769727215277637cc17047fadc33
Arman Issayev <issayevarman@mail.ru>
2024-03-02 08:53:42 +0500

    LightWeight added

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,11 +1,20 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private int id;
     private String username;
-    private int age;
-    private double balance;
+    private Type type;
+
+    public User(int id, String username, int age, double balance) {
+        Type type = LightWeight.getType(age, balance);
+        this.id=  id;
+        this.username = username;
+        this.type = type;
+    }
 
     // Getters
     public int getId() {
@@ -17,28 +26,45 @@ public class User {
     }
 
     public int getAge() {
-        return age;
+        return type.getAge();
     }
 
     public double getBalance() {
-        return balance;
+        return type.getBalance();
     }
+}
 
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
+class Type {
+    private int age;
+    private double balance;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setAge(int age) {
+    public Type(int age, double balance) {
         this.age = age;
-    }
-
-    public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    public int getAge() { return age; }
+    public double getBalance() { return balance; }
+}
+
+class LightWeight {
+    private static List<Type> typesList = new ArrayList<>();
+
+    public static Type getType(int age, double balance) {
+        Type type = findInList(age, balance);
+        if (type == null) {
+            type = new Type(age, balance);
+            typesList.add(type);
+        }
+        return type;
+    }
+
+    private static Type findInList(int age, double balance) {
+        for (Type curType : typesList) {
+            if (curType.getAge() == age && curType.getBalance() == balance) {
+                return curType;
+            }
+        }
+        return null;
+    }
 }
